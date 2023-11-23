33d86792b37aed5809442caa1ad05cd73e3a4ef6
Kimitaka Yamamoto <yamakimi1104@icloud.com>
2023-11-24 01:54:44 +0900

    pushnew”

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // main.js
 
 const { nowInSec, SkyWayAuthToken, SkyWayContext, SkyWayRoom, SkyWayStreamFactory, uuidV4 } = skyway_room;
@@ -63,73 +64,83 @@ document.addEventListener('DOMContentLoaded', function () {
 
 (async () => {
     const localVideo = document.getElementById('local-video');
-    const buttonArea = document.getElementById('button-area');
     const remoteMediaArea = document.getElementById('remote-media-area');
     const channelNameInput = document.getElementById('channel-name');
-    const dataStreamInput = document.getElementById('data-stream');
     const myId = document.getElementById('my-id');
     const joinButton = document.getElementById('join');
-    const writeButton = document.getElementById('write');
-
 
     const { audio, video } =
         await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();
     video.attach(localVideo);
     await localVideo.play();
 
-    const data = await SkyWayStreamFactory.createDataStream();
-    writeButton.onclick = () => {
-        data.write(dataStreamInput.value);
-        dataStreamInput.value = '';
-    };
+
 
     joinButton.onclick = async () => {
         if (channelNameInput.value === '') return;
-    
+
         const context = await SkyWayContext.Create(token);
         const channel = await SkyWayRoom.FindOrCreate(context, {
             type: 'p2p',
             name: channelNameInput.value,
         });
         const me = await channel.join();
-    
+
         myId.textContent = me.id;
-    
+
         await me.publish(audio);
         await me.publish(video);
-        await me.publish(data);
-    
+        //await me.publish(data);
+
+
         const subscribeAndAttach = async (publication) => {
             if (publication.publisher.id === me.id) {
                 return;
             }
-        
+
             // 自動的に購読
             const { stream } = await me.subscribe(publication.id);
-        
+
             switch (stream.contentType) {
                 case 'video':
                     {
                         const videoContainer = document.createElement('div');
                         videoContainer.classList.add('video-container');
-        
+
                         const elm = document.createElement('video');
                         elm.playsInline = true;
                         elm.autoplay = true;
                         elm.classList.add('video-element');
                         stream.attach(elm);
-        
+
                         videoContainer.appendChild(elm);
                         remoteMediaArea.appendChild(videoContainer);
                     }
                     break;
-                // ... （他のケースは変更なし）
+                case 'audio':
+                    {
+                        const elm = document.createElement('audio');
+                        elm.controls = true;
+                        elm.autoplay = true;
+                        stream.attach(elm);
+                        remoteMediaArea.appendChild(elm);
+                    }
+                    break;
+                case 'data': {
+                    const elm = document.createElement('div');
+                    remoteMediaArea.appendChild(elm);
+                    elm.innerText = 'data\n';
+                    stream.onData.add((data) => {
+                        elm.innerText += data + '\n';
+                    });
+                }
+
             }
         };
-        
+
         // 初回の既存ユーザーのストリームを購読
         channel.publications.forEach(subscribeAndAttach);
-        
+
         // 新しいユーザーがRoomに参加したときのイベントリスナー
         channel.onStreamPublished.add((e) => subscribeAndAttach(e.publication));
     };
